561ca4faa13b90091c18f9188ecfc23754d3d3d5
Kara <schatzk@xavier.edu>
2023-10-22 00:49:20 -0400

    added teaching details and LOTS of styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,33 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function clickDown(boxName) {
+  let box = document.getElementById(boxName + "-collapsable-list");
+  let img = document.getElementById(boxName + "-dropdown-icon");
+  if (box.style.display === "none") {
+    box.style.display = "block";
+    img.src = "assets/chevron-down.png";
+  } else {
+    box.style.display = "none";
+    img.src = "assets/chevron-right.png";
+  }
+}
+
+function clickRight(boxName) {
+  let box = document.getElementById(boxName + "-collapsable-list");
+  let img = document.getElementById(boxName + "-dropdown-icon");
+  let grid = document.getElementById(boxName + "-d-grid")
+  let bigBox = document.getElementById(boxName + "-collapsable-box")
+  if (box.style.display === "none") {
+    box.style.display = "block";
+    img.src = "assets/chevron-left.png";
+    grid.style.gridTemplateColumns = "45% 45% 10%";
+    bigBox.style.width = "100%";
+  } else {
+    box.style.display = "none";
+    img.src = "assets/chevron-right.png";
+    grid.style.gridTemplateColumns = "90% 10%";
+    bigBox.style.width = "50%";
+  }
+}
